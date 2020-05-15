a22155e6b86d1088f426e79cfbe9ab77758efb9d
Bryan Miller <bmiller1008@gmail.com>
2020-05-15 13:07:08 -0400

    Fixing instancing of the GlowDot color. Created a 'pusher' that will push the player in a defined direction.

--- Data/Scripts/PulseArrow.gd
+++ Data/Scripts/PulseArrow.gd
@@ -4,14 +4,18 @@ export var pulse_color:Color = Color(1.0, 0.8, 0.0, 1.0)
 
 
 func _ready():
-	$Dot.pulse_color = pulse_color
+	set_color(pulse_color)
 	$Dot.start()
-	$Dot21.pulse_color = pulse_color
 	$Dot21.start()
-	$Dot22.pulse_color = pulse_color	
 	$Dot22.start()
-	$Dot31.pulse_color = pulse_color
 	$Dot31.start()
-	$Dot32.pulse_color = pulse_color
 	$Dot32.start()
+
+func set_color(c):
+	pulse_color = c
+	$Dot.pulse_color = c
+	$Dot21.pulse_color = c
+	$Dot22.pulse_color = c
+	$Dot31.pulse_color = c
+	$Dot32.pulse_color = c
 
